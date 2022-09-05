45564ad2814625df70e4a06d10f593bf7687b0a3
Joel Catarino <joelscatarino@outlook.com>
2022-09-05 11:09:00 -0300

    first_commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,90 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from "react";
+import { useState } from "react";
+
+const api = {
+  key: "61d071fc066604ae8150d325cdb886cb",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = (e) => {
+    if (e.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery("");
+          console.log(result);
+        });
+    }
+  };
+
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <h1>Weather App</h1>
+      <div className="mt-5">
+        <input
+          className="btn"
+          type="text"
+          placeholder="search"
+          onChange={(e) => setQuery(e.target.value)}
+          value={query}
+          onKeyPress={search}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {typeof weather.main != "undefined" ? (
+        <div>
+          <div className="container-location mt-3">
+            <div className="location">
+              {weather.name}, {weather.sys.country}
+            </div>
+            <div className="clouds">Clouds: {weather.clouds.all}</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+          <div className="container-weather">
+            <div className="temp">Temperature: {weather.main.temp} C°</div>
+            <div className="weather"></div>
+          </div>
+        </div>
+      ) : ("")}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
